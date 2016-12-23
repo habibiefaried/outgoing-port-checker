6f0cf2871badd38a1e3d1a9c4a04d9fe08bcec1e
Habibie Faried <habibiefaried@gmail.com>
2016-12-23 19:02:20 +0700

    outbound_port_checker okay

--- outbound_port_checker.py
+++ outbound_port_checker.py
@@ -20,10 +20,34 @@ def time_limit(seconds):
 print "Welcome to my outbound port checker"
 print "You want to do back connect tcp reverse shell, right? ;)"
 
-timeout = int(raw_input('Set Timeout [3]: '))
-begin_port = int(raw_input('Beginning Port [1]: '))
-end_port = int(raw_input('Beginning Port [65535]: '))
-is_stop = raw_input("Write 'y' if you want to stop search when a port is already opened: ")
+try:
+	timeout = int(raw_input('Set Timeout [3]: '))
+except ValueError:
+	timeout = 3
+
+try:
+	begin_port = int(raw_input('Beginning Port [1]: '))
+except ValueError:
+	begin_port = 1
+
+try:
+	end_port = int(raw_input('Beginning Port [65535]: '))
+except ValueError:
+	end_port = 65535
+
+is_stop = raw_input("Write 'y' if you want to stop search when a port is already opened [n]: ")
+
+print "========================================="
+print "Configuration"
+print "========================================="
+print "Timeout: "+str(timeout)
+print "Begin Port: "+str(begin_port)
+print "Ending Port: "+str(end_port)
+
+if (is_stop == "y"):
+	print "[!] Scanner will stop whenever an opened port found"
+else:
+	print "[!] Scanner will do the scan from beginning port until the end"
 
 for num in range(begin_port,end_port):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
